9cb81c6a53ba2542e6e797fd4d229e4e5fc095e6
ikemia971 <alen@siwo.com>
2023-03-07 21:52:11 -0400

    ii

--- app/javascript/dashboard/components/layout/sidebarComponents/AccountContext.vue
+++ app/javascript/dashboard/components/layout/sidebarComponents/AccountContext.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    v-if="ashowShowCurrentAccountContext"
+    v-if="showShowCurrentAccountContext"
     class="account-context--group"
     @mouseover="setShowSwitch"
     @mouseleave="resetShowSwitch"
